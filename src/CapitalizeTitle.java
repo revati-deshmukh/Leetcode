9ea68b6746bc489af22f38b510f08116990bae95
revati-deshmukh <deshmukh.revat@gmail.com>
2022-03-13 17:58:12 +0800

    Added examples for Capitalize title

--- src/CapitalizeTitle.java
+++ src/CapitalizeTitle.java
@@ -1,10 +1,32 @@
 
-class Solution {
+class CapitalizeTitle {
 
     public static void main(String[] args){
-        Solution result = new Solution();
+        CapitalizeTitle result = new CapitalizeTitle();
         var output = result.capitalizeTitle("i lOve leetcode");
         System.out.println(output);
+
+        /**
+         *
+         * Example 1:
+         * Input: title = "capiTalIze tHe titLe"
+         * Output: "Capitalize The Title"
+         * Explanation:
+         * Since all the words have a length of at least 3, the first letter of each word is uppercase, and the remaining letters are lowercase.
+         *
+         * Example 2:
+         * Input: title = "First leTTeR of EACH Word"
+         * Output: "First Letter of Each Word"
+         * Explanation:
+         * The word "of" has length 2, so it is all lowercase.
+         * The remaining words have a length of at least 3, so the first letter of each remaining word is uppercase, and the remaining letters are lowercase.
+         *
+         * Example 3:
+         * Input: title = "i lOve leetcode"
+         * Output: "i Love Leetcode"
+         * Explanation:
+         * The word "i" has length 1, so it is lowercase.
+         * The remaining words have a length of at least 3, so the first letter of each remaining word is uppercase, and the remaining letters are lowercase.*/
     }
 
     public String capitalizeTitle(String title) {
