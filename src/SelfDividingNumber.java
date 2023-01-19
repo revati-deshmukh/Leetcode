364edb845f7df5c69f79ae57f925753cd3d674af
revati-deshmukh <deshmukh.revat@gmail.com>
2023-01-19 19:57:28 +0000

    Added solution for self dividing number

---
+++ src/SelfDividingNumber.java
@@ -0,0 +1,35 @@
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfDividingNumber {
+    public static void main(String[] args){
+        SelfDividingNumber sdn = new SelfDividingNumber();
+        sdn.selfDividingNumbers(11, 45);
+    }
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for(int i = left; i < right; i++){
+            if(isDivisible(i)){
+                ans.add(i);
+            }
+        }
+        System.out.println(ans);
+        return ans;
+    }
+
+    public boolean isDivisible(int num){
+        int temp = num;
+        while(num != 0){
+            int rem = num % 10;
+            if(rem == 0)
+                return false;
+            if(temp % rem != 0){
+                return false;
+            }
+            num = num / 10;
+        }
+        return true;
+    }
+}
